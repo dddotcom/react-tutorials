f402b8ddc6644995ff9abf12ef0a4223982fb73e
dddotcom <dddotcom@gmail.com>
2017-06-05 18:42:17 -0700

    practice using the weather api

--- blog_post/src/FavoriteMovies.js
+++ blog_post/src/FavoriteMovies.js
@@ -5,30 +5,59 @@ class FavoriteMovies extends Component {
   constructor(props){
     super(props);
     this.state = {
-      value: ''
+      value: '',
+      movie: '',
+      error: ''
     }
   }
 
-  componentDidMount(){
-    let url = 'http://netflixroulette.net/api/api.php?title=a%20fistful%20of%20dollars';
-
+  sanitizeInput(str){
+    return str.replace(/\s/g, "%20" );
   }
 
   handleChange(e) {
+    let str = this.sanitizeInput(e.target.value);
     this.setState(
-      {value: e.target.value}
+      {value: str}
     );
   }
 
+  formSubmit(e){
+    this.searchMovie();
+    e.preventDefault();
+  }
+
+  searchMovie(){
+    let base = this;
+    fetch(`http://netflixroulette.net/api/api.php?title=${base.state.value}`)
+      .then((response) => {
+        return response.json();
+      }).then((json) => {
+        base.setState({movie: json});
+      })
+      .catch((ex) => {
+        console.log("base.setstate", ex);
+        base.setState({error: ex});
+      });
+  }
+
   render(){
+    let m = this.state.movie;
     return (
       <div>
-        <form>
+        <form onSubmit={(e) => {this.formSubmit(e)}}>
           <label>Name:
-            <input type="text" name="name" value={this.state.value} onChange={(e) => {this.handleChange(e)}} />
+            <input type="text" name="name" onChange={(e) => {this.handleChange(e)}} />
           </label>
           <input type="submit" value="Get this movie!" />
         </form>
+        <br/>
+        {this.state.error}
+        {m.show_title}
+        <br/><br/>
+        {m.director}
+        <br/><br/>
+        {m.summary}
       </div>
     );
   }
